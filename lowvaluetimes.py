bc46ffdfc837ee5d143725c49e6da60eb2db1906
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-10-27 13:58:27 -0700

    low times measured

--- lowvaluetimes.py
+++ lowvaluetimes.py
@@ -1,5 +1,7 @@
 import time, timeit
 from changeslow import changeslow
+from changedp import changedp
+from changegreedy import changegreedy
 
 
 def avg(l): return sum(l)/len(l)
@@ -10,7 +12,20 @@ def cointime(values,amount):
   intime = time.clock()
   coins = changeslow(values,amount)
   outime = time.clock() - intime
-  return sum(coins),outime
+  return sum(coins),outime*1000
+
+def cointimedp(values,amount):
+  outtime = timeit.timeit(stmt='changedp.changedp(%s,%s)'%(str(values),str(amount)),setup="import changedp",timer=time.clock,number=100)*10
+  coins = changedp(values,amount)
+  return sum(coins),outtime
+
+def cointimegreedy(values,amount):
+  outtime = timeit.timeit(stmt='changegreedy.changegreedy(%s,%s)'%(str(values),str(amount)),setup="import changegreedy",timer=time.clock,number=1000)
+  coins = changedp(values,amount)
+  return sum(coins),outtime
+
+cointime = cointimegreedy
+
 
 valueset = [
 [1,5,10,25,50],
@@ -23,7 +38,7 @@ print "amount,",
 for v in valueset[:-1]: print "\"%s:Coins\",\"%s:Time\","%(str(v),str(v)),
 print "\"%s:Coins\",\"%s:Time\""%(str(valueset[-1]),str(valueset[-1]))
 
-for amount in range(10,500,5):
+for amount in range(10,31) + [35]:
   print amount,",",
   for v in valueset[:-1]: print "%d,%f,"%cointime(v,amount),
   print "%d,%f"%cointime(valueset[-1],amount)
