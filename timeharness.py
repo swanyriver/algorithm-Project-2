9591189b57785adedfd1632d7a4dab77c73f96eb
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-10-26 23:53:53 -0700

    this is what will run overnight

--- timeharness.py
+++ timeharness.py
@@ -1,13 +1,46 @@
 import time, timeit
+from changeslow import changeslow
 
 
 def avg(l): return sum(l)/len(l)
 
 #avg(timeit.Timer(stmt='changedp.changedp([1,5,10,25],186)',setup="import changedp",timer=time.clock).repeat(10,1))
 
-TestCases=["[1,2,4,8],15","[1,3,7,12],29","[1,3,7,12],31"]
-for tc in TestCases:
-  print timeit.timeit(stmt='changeslow.changeslow(%s)'%tc,setup="import changeslow",timer=time.clock,number=1)
+def cointime(values,amount):
+  intime = time.clock()
+  coins = changeslow(values,amount)
+  outime = time.clock() - intime
+  return sum(coins),outime
+
+valueset = [
+[1,5,10,25,50],
+[1,2,6,12,24,48,60],
+[1, 6, 13, 37, 150],
+[1] + range(2,31,2), 
+]
+
+print "amount,",
+for v in valueset[:-1]: print "\"%s:Coins\",\"%s:Time\","%(str(v),str(v)),
+print "\"%s:Coins\",\"%s:Time\""%(str(valueset[-1]),str(valueset[-1]))
+
+for amount in range(10,500,5):
+  print amount,",",
+  for v in valueset[:-1]: print "%d,%f,"%cointime(v,amount),
+  print "%d,%f"%cointime(valueset[-1],amount)
+
+
+
+# TestCases=[("[1,2,4,8],15", [1,2,4,8],15),("[1,3,7,12],29",[1,3,7,12],29),("[1,3,7,12],31",[1,3,7,12],31)]
+# for tc,values,amount in TestCases:
+#   print timeit.timeit(stmt='changeslow.changeslow(%s)'%tc,setup="import changeslow",timer=time.clock,number=1),
+
+#   intime = time.clock()
+#   changeslow(values,amount)
+#   outime = time.clock() - intime
+
+#   print " vs %f  measured manually"%outime
+
+
 
 #PROBLEM 4
 # coins = "[1,5,10,25,50],"
