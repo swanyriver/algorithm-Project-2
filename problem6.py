f83fabd865777a5a75ec4d6227f4133eefd01965
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-10-28 16:30:15 -0700

    refined greedy measurment

--- problem6.py
+++ problem6.py
@@ -4,11 +4,14 @@ from changegreedy import changegreedy
 
 values = [1] + range(2,31,2)
 
-print "amount, greedy coins, greedy time, dp coins, dp time"
+#print "amount, greedy coins, greedy time, dp coins, dp time"
+
+# for amount in range(2000,2201):
+#   print amount,",",
+#   print sum(changegreedy(values,amount)),",",
+#   print timeit.timeit(stmt='changegreedy.changegreedy(%s,%s)'%(str(values),str(amount)),setup="import changegreedy",timer=time.clock,number=1000),",",
+#   print sum(changedp(values,amount)),",",
+#   print timeit.timeit(stmt='changedp.changedp(%s,%s)'%(str(values),str(amount)),setup="import changedp",timer=time.clock,number=100)*10
 
 for amount in range(2000,2201):
-  print amount,",",
-  print sum(changegreedy(values,amount)),",",
-  print timeit.timeit(stmt='changegreedy.changegreedy(%s,%s)'%(str(values),str(amount)),setup="import changegreedy",timer=time.clock,number=1000),",",
-  print sum(changedp(values,amount)),",",
-  print timeit.timeit(stmt='changedp.changedp(%s,%s)'%(str(values),str(amount)),setup="import changedp",timer=time.clock,number=100)*10
+  print timeit.timeit(stmt='changegreedy.changegreedy(%s,%s)'%(str(values),str(amount)),setup="import changegreedy",timer=time.clock,number=1000 * 100)/100
